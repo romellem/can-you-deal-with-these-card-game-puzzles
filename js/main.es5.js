b27e8710f0c5462bc58223a12b4fd35cab169367
romellem <romellem.git@gmail.com>
2016-12-30 15:06:27 -0600

    Adding manually compiled ES5 JS files

---
+++ js/main.es5.js
@@ -0,0 +1,166 @@
+'use strict';
+
+function ready(fn) {
+	if (document.readyState != 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+// @link https://plainjs.com/javascript/selecting/select-dom-elements-by-css-selectors-4/
+// select a list of matching elements, context is optional
+function $(selector, context) {
+	return (context || document).querySelectorAll(selector);
+}
+
+// select the first match only, context is optional
+function $1(selector, context) {
+	return (context || document).querySelector(selector);
+}
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+ready(function () {
+	var time_start, time_end;
+
+	if (window.Worker) {
+		var number_of_simulations_input;
+		var worker;
+
+		(function () {
+			var initializeWorker = function initializeWorker() {
+				worker = new Worker('./js/worker.es5.js');
+
+				worker.onmessage = function (e) {
+					if (e.data.type === 'update') {
+						$1('#results .status #wins').innerText = numberWithCommas(e.data.games.win);
+						$1('#results .status #losses').innerText = numberWithCommas(e.data.games.loss);
+					} else if (e.data.type === 'final') {
+						// Remove "loading" text
+						$1('#results .loading').innerHTML = '<em>Finished</em>';
+						$1('#results .loading').className = '';
+
+						var win_percentage = e.data.games.win / e.data.number_of_simulations * 100;
+						$1('#results .status').innerHTML = '<ul>\n\t\t\t\t\t\t<li>Wins: ' + numberWithCommas(e.data.games.win) + '</li>\n\t\t\t\t\t\t<li>Losses: ' + numberWithCommas(e.data.games.loss) + '</li>\n\t\t\t\t\t\t<li>Win Probability: <strong>' + win_percentage.toFixed(3) + '%</strong>\n\t\t\t\t\t</ul>';
+
+						$1('#win-probability').innerHTML = '<h1 class="probability"><strong>' + win_percentage.toFixed(3) + '%</strong> chance you will win this game.</h1>';
+
+						$1('#win-probability').scrollIntoView();
+
+						resetButtons();
+
+						// Create animated checkmark
+						var checkmark = document.createElement('div');
+						checkmark.className = 'checkmark-circle';
+						checkmark.id = 'animated-checkmark';
+						var background = document.createElement('div');
+						background.className = 'background';
+						var draw = document.createElement('div');
+						draw.className = 'checkmark draw';
+
+						checkmark.appendChild(background);
+						checkmark.appendChild(draw);
+
+						$1('#form').appendChild(checkmark);
+
+						// Get execution time
+						if (typeof performance.now !== 'undefined') {
+							time_end = performance.now();
+							console.log('Executed in ' + (time_end - time_start) + ' milliseconds');
+						}
+					}
+				};
+			};
+
+			var resetButtons = function resetButtons() {
+				// Enable button
+				$1('#submit-button').className = 'btn btn-primary';
+				$1('#running-icon-indicator').className = "icon-right-circled";
+				$1('#submit-button .text').innerText = "Run my simulations";
+
+				// Remove cancel button
+				if ($1('#cancel-button')) {
+					$1('#form').removeChild($1('#cancel-button'));
+				}
+			};
+
+			var resetResults = function resetResults() {
+				$1('#results').innerHTML = '';
+			};
+
+			var onFormSubmit = function onFormSubmit(e) {
+				e.preventDefault();
+				if (typeof performance.now !== 'undefined') {
+					time_start = performance.now();
+				}
+
+				// Get number of simulations (default is 100 if they enter a bad value)
+				var number_of_simulations = parseInt(number_of_simulations_input.getRawValue());
+				if (!(number_of_simulations > 0)) {
+					number_of_simulations = 100;
+					number_of_simulations_input.setRawValue(number_of_simulations);
+				}
+
+				$1('#results').innerHTML = '<div class="loading">Running</div>\n\t\t\t\t<div class="status">\n\t\t\t\t\t<ul>\n\t\t\t\t\t<li>Wins: <span id="wins"></span></li>\n\t\t\t\t\t<li>Losses: <span id="losses"></span></li>\n\t\t\t\t</ul>\n\t\t\t</div>';
+
+				// Set icon to hourglass
+				$1('#running-icon-indicator').className = "icon-hourglass";
+
+				// Remove animated checkmark (if it exists)
+				if ($1('#animated-checkmark')) {
+					$1('#form').removeChild($1('#animated-checkmark'));
+				}
+
+				$1('#win-probability').innerHTML = '';
+				$1('#win-probability').scrollIntoView();
+
+				$1('#submit-button').className += " disabled";
+				$1('#submit-button .text').innerText = "Running";
+
+				// Add "Cancel" button
+				var cancel = document.createElement('button');
+				cancel.className = 'btn btn-danger';
+				cancel.id = 'cancel-button';
+				var cancel_text = document.createTextNode("Cancel");
+				cancel.appendChild(cancel_text);
+
+				$1('#form').appendChild(cancel);
+
+				$1('#cancel-button').addEventListener('click', function (e) {
+					e.preventDefault();
+
+					worker.terminate();
+					resetResults();
+					resetButtons();
+
+					// Recreate worker
+					initializeWorker();
+				});
+
+				worker.postMessage([number_of_simulations]);
+			};
+
+			// Style numeral input
+			number_of_simulations_input = new Cleave('#simulations', {
+				numeral: true,
+				numeralThousandsGroupStyle: 'thousand'
+			});
+
+			// Browser supports Web Workers
+
+			initializeWorker();
+
+			$1('#submit-button').addEventListener('click', onFormSubmit);
+			$1('#simulations').addEventListener('keypress', function (e) {
+				if (e.which == 13 || e.keyCode == 13) {
+					onFormSubmit(e);
+				}
+			});
+		})();
+	} else {
+		$1('#main').innerHTML = 'Sorry, your web browser must support ' + '<a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API">Web Workers</a> ' + 'in order to run this page.';
+	}
+});
---
+++ js/worker.es5.js
@@ -0,0 +1,121 @@
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var CARDS = {
+	0: 'A♠',
+	1: '2♠',
+	2: '3♠',
+	3: '4♠',
+	4: '5♠',
+	5: '6♠',
+	6: '7♠',
+	7: '8♠',
+	8: '9♠',
+	9: 'T♠',
+	10: 'J♠',
+	11: 'Q♠',
+	12: 'K♠',
+	13: 'A♥',
+	14: '2♥',
+	15: '3♥',
+	16: '4♥',
+	17: '5♥',
+	18: '6♥',
+	19: '7♥',
+	20: '8♥',
+	21: '9♥',
+	22: 'T♥',
+	23: 'J♥',
+	24: 'Q♥',
+	25: 'K♥',
+	26: 'A♦',
+	27: '2♦',
+	28: '3♦',
+	29: '4♦',
+	30: '5♦',
+	31: '6♦',
+	32: '7♦',
+	33: '8♦',
+	34: '9♦',
+	35: 'T♦',
+	36: 'J♦',
+	37: 'Q♦',
+	38: 'K♦',
+	39: 'A♣',
+	40: '2♣',
+	41: '3♣',
+	42: '4♣',
+	43: '5♣',
+	44: '6♣',
+	45: '7♣',
+	46: '8♣',
+	47: '9♣',
+	48: 'T♣',
+	49: 'J♣',
+	50: 'Q♣',
+	51: 'K♣'
+};
+
+function getCard(num) {
+	return typeof CARDS[num] !== 'undefined' ? CARDS[num] : null;
+}
+
+function shuffle(array) {
+	var currentIndex = array.length,
+	    temporaryValue,
+	    randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function getShuffledDeck() {
+	return shuffle([].concat(_toConsumableArray(Array(52).keys())));
+}
+
+self.addEventListener('message', function (e) {
+	var number_of_simulations = e.data[0];
+	console.log('Running ' + number_of_simulations + ' times');
+
+	var games = { win: 0, loss: 0 };
+
+	var deck = [].concat(_toConsumableArray(Array(52).keys()));
+	for (var i = 0; i < number_of_simulations; i++) {
+		deck = shuffle(deck);
+
+		var status = 'win';
+		deck.some(function (card, index) {
+			if (card % 13 === index % 13) {
+				// console.log(getCard(card) + ' ' + getCard(index));
+				status = 'loss';
+
+				// Break out of our loop by returning "true"
+				return true;
+			}
+		});
+
+		// Tally our win or loss
+		games[status] += 1;
+
+		// Send an update back every 100 simulations
+		if (i % 100 === 0) {
+			postMessage({ type: 'update', games: games, number_of_simulations: number_of_simulations });
+		}
+	}
+
+	postMessage({ type: 'final', games: games, number_of_simulations: number_of_simulations });
+	// close();
+}, false);
