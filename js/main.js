e31fdb7f4469788933df0d71ff44010ce9012934
romellem <romellem.git@gmail.com>
2016-12-30 13:55:43 -0600

    Cleanup, looking better
    Trying to get execution time working...

--- js/main.js
+++ js/main.js
@@ -9,16 +9,25 @@ function ready(fn) {
 // @link https://plainjs.com/javascript/selecting/select-dom-elements-by-css-selectors-4/
 // select a list of matching elements, context is optional
 function $(selector, context) {
-    return (context || document).querySelectorAll(selector);
+	return (context || document).querySelectorAll(selector);
 }
 
 // select the first match only, context is optional
 function $1(selector, context) {
-    return (context || document).querySelector(selector);
+	return (context || document).querySelector(selector);
+}
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 
 ready(function() {
+	var time_start, time_end;
+	if (!window.Performance) {
+		var performance = {now: function(){return 0;}};
+	}
+
 	if (window.Worker) {
 		// Style numeral input
 		var number_of_simulations_input = new Cleave('#simulations', {
@@ -31,24 +40,36 @@ ready(function() {
 
 		worker.onmessage = function(e) {
 			if (e.data.type === 'update') {
-				$1('#results .status').innerHTML = `<ul>
-					<li>Wins: ${e.data.games.win}</li>
-					<li>Losses: ${e.data.games.loss}</li>
-				</ul>`;
+				$1('#results .status #wins').innerText = numberWithCommas(e.data.games.win);
+				$1('#results .status #losses').innerText = numberWithCommas(e.data.games.loss);
 			} else if (e.data.type === 'final') {
+				// Remove "loading" text
+				$1('#results .loading').innerHTML = '<em>Finished</em>';
+				$1('#results .loading').className = '';
+
+				var win_percentage = e.data.games.win / e.data.number_of_simulations * 100;
 				$1('#results .status').innerHTML = `<ul>
-					<li>Wins: ${e.data.games.win}</li>
-					<li>Losses: ${e.data.games.loss}</li>
-					<li>Win Probability: ${e.data.games.win / e.data.number_of_simulations}
+					<li>Wins: ${numberWithCommas(e.data.games.win)}</li>
+					<li>Losses: ${numberWithCommas(e.data.games.loss)}</li>
+					<li>Win Probability: <strong>${win_percentage.toFixed(3)}%</strong>
 				</ul>`;
+
+				$1('#win-probability').innerHTML = '<h1 class="probability"><strong>' + win_percentage.toFixed(3) + '%</strong> chance you will win this game.</h1>';
+
+				$1('#win-probability').scrollIntoView();
 
 				// Enable button
 				$1('#submit-button').className = 'btn btn-primary';
+
+				// Get execution time
+				// time_end = performance.now();
+				// console.log('Executed in ' + (time_end - time_start) + ' milliseconds');
 			}
 		};
 
 		$1('#submit-button').addEventListener('click', function(e) {
 			e.preventDefault();
+			// time_start = performance.now();
 
 			// Get number of simulations (default is 100 if they enter a bad value)
 			var number_of_simulations = parseInt(number_of_simulations_input.getRawValue());
@@ -57,15 +78,21 @@ ready(function() {
 				number_of_simulations_input.setRawValue(number_of_simulations);
 			}
 
-			$1('#results').innerHTML = '<div class="loading">Running</div><div class="status"></div>';
-			this.className += " disable";
+			$1('#results').innerHTML = `<div class="loading">Running</div>
+				<div class="status">
+					<ul>
+					<li>Wins: <span id="wins"></span></li>
+					<li>Losses: <span id="losses"></span></li>
+				</ul>
+			</div>`;
+
+			$1('#win-probability').innerHTML = '';
+			$1('#win-probability').scrollIntoView();
+
+			this.className += " disabled";
 
 			worker.postMessage([number_of_simulations]);
-		});
-
-		
-
-		
+		});		
 	} else {
 		$1('#main').innerHTML = 'Sorry, your web browser must support ' + 
 			'<a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API">Web Workers</a> ' + 
--- js/worker.js
+++ js/worker.js
@@ -1,83 +1,83 @@
 const CARDS = {
-  0 : 'A♠',
-  1 : '2♠',
-  2 : '3♠',
-  3 : '4♠',
-  4 : '5♠',
-  5 : '6♠',
-  6 : '7♠',
-  7 : '8♠',
-  8 : '9♠',
-  9 : 'T♠',
-  10: 'J♠',
-  11: 'Q♠',
-  12: 'K♠',
-  13: 'A♥',
-  14: '2♥',
-  15: '3♥',
-  16: '4♥',
-  17: '5♥',
-  18: '6♥',
-  19: '7♥',
-  20: '8♥',
-  21: '9♥',
-  22: 'T♥',
-  23: 'J♥',
-  24: 'Q♥',
-  25: 'K♥',
-  26: 'A♦',
-  27: '2♦',
-  28: '3♦',
-  29: '4♦',
-  30: '5♦',
-  31: '6♦',
-  32: '7♦',
-  33: '8♦',
-  34: '9♦',
-  35: 'T♦',
-  36: 'J♦',
-  37: 'Q♦',
-  38: 'K♦',
-  39: 'A♣',
-  40: '2♣',
-  41: '3♣',
-  42: '4♣',
-  43: '5♣',
-  44: '6♣',
-  45: '7♣',
-  46: '8♣',
-  47: '9♣',
-  48: 'T♣',
-  49: 'J♣',
-  50: 'Q♣',
-  51: 'K♣'
+	0 : 'A♠',
+	1 : '2♠',
+	2 : '3♠',
+	3 : '4♠',
+	4 : '5♠',
+	5 : '6♠',
+	6 : '7♠',
+	7 : '8♠',
+	8 : '9♠',
+	9 : 'T♠',
+	10: 'J♠',
+	11: 'Q♠',
+	12: 'K♠',
+	13: 'A♥',
+	14: '2♥',
+	15: '3♥',
+	16: '4♥',
+	17: '5♥',
+	18: '6♥',
+	19: '7♥',
+	20: '8♥',
+	21: '9♥',
+	22: 'T♥',
+	23: 'J♥',
+	24: 'Q♥',
+	25: 'K♥',
+	26: 'A♦',
+	27: '2♦',
+	28: '3♦',
+	29: '4♦',
+	30: '5♦',
+	31: '6♦',
+	32: '7♦',
+	33: '8♦',
+	34: '9♦',
+	35: 'T♦',
+	36: 'J♦',
+	37: 'Q♦',
+	38: 'K♦',
+	39: 'A♣',
+	40: '2♣',
+	41: '3♣',
+	42: '4♣',
+	43: '5♣',
+	44: '6♣',
+	45: '7♣',
+	46: '8♣',
+	47: '9♣',
+	48: 'T♣',
+	49: 'J♣',
+	50: 'Q♣',
+	51: 'K♣'
 };
 
 function getCard(num) {
-  return (typeof CARDS[num] !== 'undefined' ? CARDS[num] : null);
+	return (typeof CARDS[num] !== 'undefined' ? CARDS[num] : null);
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-  return array;
+	return array;
 }
 
 function getShuffledDeck() {
-  return shuffle([...Array(52).keys()]);
+	return shuffle([...Array(52).keys()]);
 }
 
 onmessage = function(e) {
@@ -87,24 +87,26 @@ onmessage = function(e) {
 	var games = {win: 0, loss: 0};
 
 	for (let i = 0; i < number_of_simulations; i++) {
-	  let deck = getShuffledDeck();
+		let deck = getShuffledDeck();
 
-	  let status = 'win';
-	  deck.some((card, index) => {
-	    if (card % 13 === index % 13) {
-	      // console.log(getCard(card) + ' ' + getCard(index));
-	      status = 'loss';
+		let status = 'win';
+		deck.some((card, index) => {
+			if (card % 13 === index % 13) {
+				// console.log(getCard(card) + ' ' + getCard(index));
+				status = 'loss';
 
-	      // Break out of our loop by returning "true"
-	      return true;
-	    }
-	  });
+				// Break out of our loop by returning "true"
+				return true;
+			}
+		});
 
-	  // Tally our win or loss
-	  games[status] += 1;
+		// Tally our win or loss
+		games[status] += 1;
 
-	  // Send an update back
-	  postMessage({type: 'update', games: games, number_of_simulations: number_of_simulations});
+		// Send an update back every 25 simulations
+		if (i % 25 === 0) {
+			postMessage({type: 'update', games: games, number_of_simulations: number_of_simulations});
+		}
 	}
 
 	postMessage({type: 'final', games: games, number_of_simulations: number_of_simulations});
