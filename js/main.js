e1ee5d2d8a41686cc105db9c97126f1494c9f856
romellem <romellem.git@gmail.com>
2016-12-30 12:02:54 -0600

    Break long string to multiple lines

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ ready(function() {
 
 		worker.postMessage([5]);
 	} else {
-		document.getElementById('main').innerHTML = 'Sorry, your web browser must support <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API">Web Workers</a> in order to run this page.';
+		document.getElementById('main').innerHTML = 'Sorry, your web browser must support ' + 
+			'<a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API">Web Workers</a>' + 
+			'in order to run this page.';
 	}
 });
