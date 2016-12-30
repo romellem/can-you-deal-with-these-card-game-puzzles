0dcef2970355c28aebae1a54780a9e289dddb898
romellem <romellem.git@gmail.com>
2016-12-30 12:47:07 -0600

    Starting to get form together

--- js/main.js
+++ js/main.js
@@ -6,9 +6,32 @@ function ready(fn) {
 	}
 }
 
+// @link https://plainjs.com/javascript/selecting/select-dom-elements-by-css-selectors-4/
+// select a list of matching elements, context is optional
+function $(selector, context) {
+    return (context || document).querySelectorAll(selector);
+}
+
+// select the first match only, context is optional
+function $1(selector, context) {
+    return (context || document).querySelector(selector);
+}
+
 
 ready(function() {
 	if (window.Worker) {
+		// Style numeral input
+		var cleave = new Cleave('#simulations', {
+			numeral: true,
+			numeralThousandsGroupStyle: 'thousand'
+		});
+
+		$1('#submit-button').addEventListener('click', function(e) {
+			e.preventDefault();
+
+			console.log('hello');
+		});
+
 		// Browser supports Web Workers
 		var worker = new Worker('./js/worker.js');
 
